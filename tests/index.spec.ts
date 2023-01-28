ce66fb2d17284982bf259d961644bd9ff351def6
4lessandrodev <alessandroadm@live.com>
2023-01-28 02:40:58 -0300

    test(log): ensure local log testing #1

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -6,12 +6,12 @@ describe('log', () => {
     it('should log to be defined', () => {
 
         const uid = randomUUID();
-
+        // create a global log
         const global = Log.init({ name: 'First Log', uid });
-        const step1 = Step.info({ message: 'Some step', name: 'Request' });
-        const a = global.addStep(step1);
-        const b = a.addStep(
-            Step.error({
+
+        // create steps
+        const info = Step.info({ message: 'Some step', name: 'Request' });
+        const error = Step.error({
             message: 'Tieout', 
             name: 'Login', 
             stack: `Error("myError")@:0
@@ -20,9 +20,13 @@ describe('log', () => {
                     first call, firsta)@file:///C:/example.html:19
                     @file:///C:/example.html:21
             `
-        }))
+        });
 
-        b.print();
-        b.writeLocal();
+        // add steps to global log
+        const err = global.addStep(info).addStep(error);
+
+        // print or save logs
+        err.print();
+        err.writeLocal();
     })
 });
