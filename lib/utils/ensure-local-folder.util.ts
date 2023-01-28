bb73dd4d60e3d33fd45f56b73c86bfd963b7373b
4lessandrodev <alessandroadm@live.com>
2023-01-28 17:41:36 -0300

    feat(local): added function to get or create local folder check if folder exists if not create it #2

---
+++ lib/utils/ensure-local-folder.util.ts
@@ -0,0 +1,19 @@
+import { existsSync, mkdirSync } from "node:fs";
+
+/**
+ * @description Checks if the given path directory exists. If it does not exist, it creates the directory for the path informed recursively.
+ * @param path dir to save log
+ * @returns true if success and false if failure.
+ */
+export const EnsureLocalFolder = async (path: string): Promise<boolean> => {
+    const existsDir = existsSync(path);
+    if (!existsDir) {
+        try {
+            mkdirSync(path, { recursive: true });
+        } catch (error) {
+            return (false);
+        }
+    }
+    return (true);
+}
+export default EnsureLocalFolder;
