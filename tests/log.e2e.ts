cff5bfca5c40d9776a55b83494c496a3dfe7844a
4lessandrodev <alessandroadm@live.com>
2023-02-07 11:33:21 -0300

    test: update e2e test

--- tests/log.e2e.ts
+++ tests/log.e2e.ts
@@ -1,11 +1,13 @@
 import { S3Credentials, SavePayload } from "../lib/types";
 import { Log } from "../lib/core/log";
 import { Config } from "../lib/core/providers";
+import { Step } from "../lib/core/step";
 
 it('should publish to http', async () => {
     jest.setTimeout(900000);
-    const log = Log.init({ name: 'Test' });
-    const url = 'https://postback-4dev.onrender.com/logs-test';
+    const step = Step.info({ name: 'Test', message: 'testing...' });
+    const log = Log.init({ name: 'Test', steps: [step] });
+    const url = 'https://postback-4dev.onrender.com/test-log';
     const config = Config.Http({ url });
     const result = await log.publish(config);
     expect(result).toEqual({ statusCode: 200, url } satisfies SavePayload);
@@ -17,16 +19,17 @@ it('should publish to s3', async () => {
     const bucketName = process.env.BUCKET_NAME!;
     const region = process.env.REGION! as any;
     const secretAccessKey = process.env.SECRET_ACCESS_KEY!;
-    
+
     expect(accessKeyId).toBeDefined()
     expect(bucketName).toBeDefined()
     expect(region).toBeDefined()
     expect(secretAccessKey).toBeDefined()
-    
+
     const credentials = { accessKeyId, secretAccessKey } satisfies S3Credentials;
     const config = Config.S3({ bucketName, credentials, region });
-    
-    const log = Log.init({ name: 'Test' });
+    const step = Step.info({ name: 'Test', message: 'testing...' });
+
+    const log = Log.init({ name: 'Test', steps: [step] });
     const result = await log.publish(config);
 
     expect(result?.statusCode).toBe(200);
